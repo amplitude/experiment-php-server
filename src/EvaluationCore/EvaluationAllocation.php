e2f7b85440f1f0dfaab896df517c3010634fb592
Tim Yiu <tim.yiu@Tims-MacBook-Pro.local>
2023-11-08 14:27:52 -0800

    remove unused evaluation classes

--- src/EvaluationCore/EvaluationAllocation.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace AmplitudeExperiment\EvaluationCore;
-
-class EvaluationAllocation
-{
-    public array $range; // An array of numbers
-    public array $distributions; // An array of EvaluationDistribution objects
-}
--- src/EvaluationCore/EvaluationDistribution.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace AmplitudeExperiment\EvaluationCore;
-
-class EvaluationDistribution
-{
-    public string $variant;
-    public array $range; // An array of numbers
-}
