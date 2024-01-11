24dd6bf371047d28987e213457fc95b46b10583a
tyiuhc <tim.yiu@amplitude.com>
2024-01-11 13:22:40 -0800

    docs: improve HttpClientInterface descriptions

--- src/Http/HttpClientInterface.php
+++ src/Http/HttpClientInterface.php
@@ -2,10 +2,12 @@
 
 namespace AmplitudeExperiment\Http;
 
-
 use Psr\Http\Client\ClientInterface;
 use Psr\Http\Message\RequestInterface;
 
+/**
+ * Interface for the HTTP clients set in {@link RemoteEvaluationConfig} and {@link LocalEvaluationConfig}.
+ */
 interface HttpClientInterface
 {
     /**
@@ -14,6 +16,9 @@ interface HttpClientInterface
     public function getClient(): ClientInterface;
     /**
      * Return a PSR Request to be sent by the underlying PSR HTTP Client
+     * @param string $method The HTTP method to use
+     * @param string $uri The URI to send the request to
+     * @param string|null $body The body of the request
      */
     public function createRequest(string $method, string $uri, ?string $body = null) : RequestInterface;
 }
