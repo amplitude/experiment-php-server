48f682a7905159f34794c103c159de9c6efd326c
tyiuhc <tim.yiu@amplitude.com>
2024-01-23 11:30:41 -0800

    Allow all versions of PSR log

--- src/Logger/InternalLogger.php
+++ src/Logger/InternalLogger.php
@@ -15,63 +15,63 @@ class InternalLogger implements LoggerInterface
         $this->logLevel = $logLevel;
     }
 
-    public function emergency($message, array $context = array())
+    public function emergency($message, array $context = []): void
     {
         if ($this->shouldLog(LogLevel::EMERGENCY)) {
             $this->logger->emergency($message, $context);
         }
     }
 
-    public function alert($message, array $context = array())
+    public function alert($message, array $context = []): void
     {
         if ($this->shouldLog(LogLevel::ALERT)) {
             $this->logger->alert($message, $context);
         }
     }
 
-    public function critical($message, array $context = array())
+    public function critical($message, array $context = []): void
     {
         if ($this->shouldLog(LogLevel::CRITICAL)) {
             $this->logger->critical($message, $context);
         }
     }
 
-    public function error($message, array $context = array())
+    public function error($message, array $context = []): void
     {
         if ($this->shouldLog(LogLevel::ERROR)) {
             $this->logger->error($message, $context);
         }
     }
 
-    public function warning($message, array $context = array())
+    public function warning($message, array $context = []): void
     {
         if ($this->shouldLog(LogLevel::WARNING)) {
             $this->logger->warning($message, $context);
         }
     }
 
-    public function notice($message, array $context = array())
+    public function notice($message, array $context = []): void
     {
         if ($this->shouldLog(LogLevel::NOTICE)) {
             $this->logger->notice($message, $context);
         }
     }
 
-    public function info($message, array $context = array())
+    public function info($message, array $context = []): void
     {
         if ($this->shouldLog(LogLevel::INFO)) {
             $this->logger->info($message, $context);
         }
     }
 
-    public function debug($message, array $context = array())
+    public function debug($message, array $context = []): void
     {
         if ($this->shouldLog(LogLevel::DEBUG)) {
             $this->logger->debug($message, $context);
         }
     }
 
-    public function log($level, $message, array $context = array())
+    public function log($level, $message, array $context = []): void
     {
         // Do nothing
     }
