5faa58fed59834ff6a6450af0b0fd6ff4571d41d
tyiuhc <tim.yiu@amplitude.com>
2024-09-09 13:41:13 -0700

    fix: Allow creation of assignment event from array

--- src/Amplitude/Event.php
+++ src/Amplitude/Event.php
@@ -4,40 +4,86 @@ namespace AmplitudeExperiment\Amplitude;
 
 use RuntimeException;
 
+/**
+ * @phpstan-type Payload array{
+ *     event_type: string,
+ *     event_properties?: array<mixed>|null,
+ *     user_properties?: array<mixed>|null,
+ *     user_id?: string|null,
+ *     device_id?: string|null,
+ *     insert_id?: string|null,
+ *     time?: int|null
+ * }
+ */
 class Event
 {
-    public ?string $eventType = null;
+    public ?string $eventType;
     /**
-     * @var ?array<mixed>
+     * @var array<mixed>|null
      */
-    public ?array $eventProperties = null;
+    public ?array $eventProperties;
     /**
-     * @var ?array<mixed>
+     * @var array<mixed>|null
      */
-    public ?array $userProperties = null;
-    public ?string $userId = null;
-    public ?string $deviceId = null;
-    public ?string $insertId = null;
-    public ?int $time = null;
+    public ?array $userProperties;
+    public ?string $userId;
+    public ?string $deviceId;
+    public ?string $insertId;
+    public ?int $time;
 
-    public function __construct(string $eventType)
+    /**
+     * @param array<mixed>|null $eventProperties
+     * @param array<mixed>|null $userProperties
+     */
+    public function __construct(
+        string $eventType,
+        ?array $eventProperties = null,
+        ?array $userProperties = null,
+        ?string $userId = null,
+        ?string $deviceId = null,
+        ?string $insertId = null,
+        ?int $time = null
+    )
     {
         $this->eventType = $eventType;
+        $this->eventProperties = $eventProperties;
+        $this->userProperties = $userProperties;
+        $this->userId = $userId;
+        $this->deviceId = $deviceId;
+        $this->insertId = $insertId;
+        $this->time = $time;
     }
 
     /**
-     * @return array<string, mixed>
+     * @return Payload
      */
     public function toArray(): array
     {
-        return array_filter([
-            'event_type' => $this->eventType,
-            'event_properties' => $this->eventProperties,
-            'user_properties' => $this->userProperties,
-            'user_id' => $this->userId,
-            'device_id' => $this->deviceId,
-            'insert_id' => $this->insertId,
-            'time' => $this->time]);
+        return [
+            'event_type' => $this->eventType ?? '',
+            'event_properties' => $this->eventProperties ?? null,
+            'user_properties' => $this->userProperties ?? null,
+            'user_id' => $this->userId ?? null,
+            'device_id' => $this->deviceId ?? null,
+            'insert_id' => $this->insertId ?? null,
+            'time' => $this->time ?? null,
+        ];
+    }
+
+    /**
+     * @param Payload $data
+     */
+    public static function fromArray(array $data) : self
+    {
+        return new self(
+            $data['event_type'],
+            $data['event_properties'] ?? null,
+            $data['user_properties'] ?? null,
+            $data['user_id'] ?? null,
+            $data['device_id'] ?? null,
+            $data['insert_id'] ?? null,
+            $data['time'] ?? null
+        );
     }
 
     /**
