0b5e108aa13a35550b0fd5d6067e60795e685a4c
Tim Yiu <137842098+tyiuhc@users.noreply.github.com>
2025-12-16 17:22:05 -0800

    fix: use string casting for messages in logger

--- src/Logger/DefaultLogger.php
+++ src/Logger/DefaultLogger.php
@@ -14,42 +14,42 @@ class DefaultLogger implements LoggerInterface
 {
     public function emergency($message, array $context = []): void
     {
-        self::logMessage(LogLevel::EMERGENCY, $message, $context);
+        self::logMessage(LogLevel::EMERGENCY, (string) $message, $context);
     }
 
     public function alert($message, array $context = []): void
     {
-        self::logMessage(LogLevel::ALERT, $message, $context);
+        self::logMessage(LogLevel::ALERT, (string) $message, $context);
     }
 
     public function critical($message, array $context = []): void
     {
-        self::logMessage(LogLevel::CRITICAL, $message, $context);
+        self::logMessage(LogLevel::CRITICAL, (string) $message, $context);
     }
 
     public function error($message, array $context = []): void
     {
-        self::logMessage(LogLevel::ERROR, $message, $context);
+        self::logMessage(LogLevel::ERROR, (string) $message, $context);
     }
 
     public function warning($message, array $context = []): void
     {
-        self::logMessage(LogLevel::WARNING, $message, $context);
+        self::logMessage(LogLevel::WARNING, (string) $message, $context);
     }
 
     public function notice($message, array $context = []): void
     {
-        self::logMessage(LogLevel::NOTICE, $message, $context);
+        self::logMessage(LogLevel::NOTICE, (string) $message, $context);
     }
 
     public function info($message, array $context = []): void
     {
-        self::logMessage(LogLevel::INFO, $message, $context);
+        self::logMessage(LogLevel::INFO, (string) $message, $context);
     }
 
     public function debug($message, array $context = []): void
     {
-        self::logMessage(LogLevel::DEBUG, $message, $context);
+        self::logMessage(LogLevel::DEBUG, (string) $message, $context);
     }
 
     public function log($level, $message, array $context = []): void
--- src/Logger/InternalLogger.php
+++ src/Logger/InternalLogger.php
@@ -18,56 +18,56 @@ class InternalLogger implements LoggerInterface
     public function emergency($message, array $context = []): void
     {
         if ($this->shouldLog(LogLevel::EMERGENCY)) {
-            $this->logger->emergency($message, $context);
+            $this->logger->emergency((string) $message, $context);
         }
     }
 
     public function alert($message, array $context = []): void
     {
         if ($this->shouldLog(LogLevel::ALERT)) {
-            $this->logger->alert($message, $context);
+            $this->logger->alert((string) $message, $context);
         }
     }
 
     public function critical($message, array $context = []): void
     {
         if ($this->shouldLog(LogLevel::CRITICAL)) {
-            $this->logger->critical($message, $context);
+            $this->logger->critical((string) $message, $context);
         }
     }
 
     public function error($message, array $context = []): void
     {
         if ($this->shouldLog(LogLevel::ERROR)) {
-            $this->logger->error($message, $context);
+            $this->logger->error((string) $message, $context);
         }
     }
 
     public function warning($message, array $context = []): void
     {
         if ($this->shouldLog(LogLevel::WARNING)) {
-            $this->logger->warning($message, $context);
+            $this->logger->warning((string) $message, $context);
         }
     }
 
     public function notice($message, array $context = []): void
     {
         if ($this->shouldLog(LogLevel::NOTICE)) {
-            $this->logger->notice($message, $context);
+            $this->logger->notice((string) $message, $context);
         }
     }
 
     public function info($message, array $context = []): void
     {
         if ($this->shouldLog(LogLevel::INFO)) {
-            $this->logger->info($message, $context);
+            $this->logger->info((string) $message, $context);
         }
     }
 
     public function debug($message, array $context = []): void
     {
         if ($this->shouldLog(LogLevel::DEBUG)) {
-            $this->logger->debug($message, $context);
+            $this->logger->debug((string) $message, $context);
         }
     }
 
